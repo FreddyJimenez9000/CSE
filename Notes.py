577622a4f550e17cbfa5dcfb7e4892dac4548ffb
FreddyJimenez9000 <33761463+FreddyJimenez9000@users.noreply.github.com>
2017-11-28 09:35:41 -0800

    Initial commit

---
+++ Notes.py
@@ -0,0 +1,31 @@
+print("Hello world")
+
+# mr. Weibe
+
+a = 3
+b = 6
+
+print(3 + 6)
+print(6 - 3)
+print(6 / 3)
+print(3 * 6)
+
+print("Try to slove this type of problem and what it mean")
+print(15 % 12 )
+# this "%" sign is a module. It find the reamander
+
+
+car_named = print("red")
+car_type = "BMW"
+car_cylinder = 8
+car_mpg = 5000.9
+
+print("I have a car called %s. It's red." % car_named)
+print("I have a car called %s. It's red." % car_named, car_type)
+
+print("What is your named?")
+name = input(">_")
+print("Hello %s." % name)
+
+age = input("How old are you?")
+print("%s?! That's really old ." % age)
