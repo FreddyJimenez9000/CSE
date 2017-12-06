94d63b47500abd786a943885535e420064bebf9f
FreddyJimenez9000 <33761463+FreddyJimenez9000@users.noreply.github.com>
2017-12-06 09:45:53 -0800

    new

--- Freddy JImenez Guess game.py
+++ Freddy JImenez Guess game.py
@@ -1,18 +1,27 @@
 import random
+
 print(random.randint(0, 50))
 
-c = '23'
+guess = (7)
 
 
-print("Try to Guess a number from one through fifty")
+num = (6)
+
+print(guess = num)
+
+guess == num
+
+input(">_")
+
+def guess(num):
+  if guess == num:
+    print("correct")
+  elif guess < num:
+    print("higher")
+  elif guess > num :
+    print("lower")
+
+input(">_")
 
 
-def number_guess(number):
-    if number == 34:
-        return"correct"
-    elif number < 34:
-        return"incorrect"
-    elif number > 34:
-        return"incorrect"
-
-
+        # generate a number, ask the user for an input(number), does the guess match the number?  #  add "higher" or "lower" , add 5 guesses
--- Warm ups.py
+++ Warm ups.py
@@ -3,6 +3,19 @@ def reverse_order(first, last):
     print(last + " " + first)  # concatenation
 
 
+def add_py(name):
+    print("%s.py" %name)
+    print(name + ".py")
+
+
+def add(number):
+    print(number + number + number)
+
+
+add(9000)
+add(100)
+
+
 
 
 
