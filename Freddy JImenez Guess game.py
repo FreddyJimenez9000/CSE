985a990b9c75a4211f30fc7f80a945c2d112b78f
FreddyJimenez9000 <33761463+FreddyJimenez9000@users.noreply.github.com>
2018-01-09 09:44:48 -0800

    the start of lucky 7

--- Freddy JImenez Guess game.py
+++ Freddy JImenez Guess game.py
@@ -1,33 +1,56 @@
 import random
 
-print(random.randint(0, 50))
 
-input(">_")
-guess = (input)
-num = ()
-
-
-def print_(num):
-  if guess == num():
-    return print("correct")
-    elif_guess = num
-    return print("go higher")
-    elif  guess >= num
-    return print("go lower")
-
-
-
-
-
-# def print_num():
-#   if guess == num:
-#      print("correct")
-#    elif guess < num:
-#      print("higher")
-#    elif guess < num :
-#      print("lower")
+# while number < 10:
+#     guess = input()
+#     guess = int(guess)
+#
+# if number < guess:
+#    print("it's low")
+# input(">_")
+# guess = input
+# num = 'import random'
 #
 
+# def print (num):
+#    if guess == num():
+#     return print("correct")
+#   elif guess < num:
+#     return print("go higher")
+#   elif guess > num:
+#     return print("go lower")
 
 
+# def number(guess_num):
+#    if guess == num:
+#     print("correct")
+#    elif str(guess) < num:
+#     print("higher")
+#    elif str(guess) < num :
+#     print("lower")
+
+
+        # generate a number, ask the user for an input(number), does the guess match the number?  #  add "higher" or "lower" , add 5 guesses
+
+guessTaken = 0
+
+number = random.randint(1, 50)
+
+
+while guessTaken < 5:
+    print("take a guess")
+    guess = int(input())
+
+    guessTaken = guessTaken + 1
+
+    if guess < number:
+        print("Your guess is too low")
+    if guess > number:
+        print("Your guess is too high")
+    if guess == number:
+        print("correct")
+
+if guess != number:
+    number = str(number)
+print("Nope. The number I was thinking of was " + number)
         # generate a number, ask the user for an input(number), does the guess match the number?  #  add "higher" or "lower" , add 5 guesses
---
+++ Freddy Jimenez lucky 7.py
@@ -0,0 +1,12 @@
+import random
+
+round = 
+roll = random.randint(2,7)
+money = 15
+
+while money < roll:
+    print("so do you want to play a game of lucky 7")
+    print("just spend a dollar to gamble your money on this rigged game")
+    roll = money - 1
+
+
