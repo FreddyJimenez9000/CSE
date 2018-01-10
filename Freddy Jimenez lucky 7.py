1be8b65547623b8fed4206fbe7d62202cf0cc5b6
FreddyJimenez9000 <33761463+FreddyJimenez9000@users.noreply.github.com>
2018-01-10 09:45:23 -0800

    making progress

--- Freddy Jimenez lucky 7.py
+++ Freddy Jimenez lucky 7.py
@@ -1,12 +1,23 @@
 import random
 
-round = 
-roll = random.randint(2,7)
-money = 15
+D1 = (random.randint(1, 6))
+D2 = (random.randint(1, 6))
 
-while money < roll:
+money = 15
+insert = -1
+win = 7
+
+while insert < D1:
     print("so do you want to play a game of lucky 7")
-    print("just spend a dollar to gamble your money on this rigged game")
-    roll = money - 1
+    print("just spend a dollar to gamble your money on this rigged game.")
+    insert = int(input())
+
+    print(D1)
+    print(D2)
+
+    if win != win:
+    print("you lost want to play again" )
+    if win == win:
+    print("you win here 4 dollars, want to play again")
 
 
