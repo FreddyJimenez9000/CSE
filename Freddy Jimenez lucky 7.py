9bb05feefc269549210a7dc370c7713327e46717
FreddyJimenez9000 <33761463+FreddyJimenez9000@users.noreply.github.com>
2018-01-17 09:45:22 -0800

    almost

--- Freddy Jimenez lucky 7.py
+++ Freddy Jimenez lucky 7.py
@@ -1,33 +1,36 @@
 import random
-
 number = 0
 money = 15
 insert = -1
 win = 7
-while insert < number:
+print("so do you want to play a game of lucky 7")
+print("you start off with $15 just spend a $1 to gamble your money to play this game.")
+while money > 0:
     D1 = (random.randint(1, 6))
     D2 = (random.randint(1, 6))
-    print("so do you want to play a game of lucky 7")
-    print("you start off with $15 just spend a $1 to gamble your money to play this game.")
-    insert = int(input())
+    stuff = int(input())
+    money -= 1
     number = number + 1
-    print(money - 1)
-
     print(D1)
     print(D2)
-
     print(D1 + D2)
 
     if (D1 + D2) == win:
         print("you win here 4 dollars, want to play again")
-        print(" your money is" + str(money + 4))
+        print(" your money is")
+        money += 5
+        print("money =",money)
+        print("round =",number)
     elif (D1 + D2) != win:
         print("you lost want to play again, just insert $1")
+        print("money =",money)
+        print("round =",number)
     elif money == 0:
-        number = str(number)
-        print("the amount of round you played" + number)
-        money = str(money)
-        print("also the money you earn" + money)
+        print("the amount of round you played")
+        print("round =", number)
+        print("also the money you earn")
+        print("money =", money)
+
 
 
 
