309dcb6a1570f1f84269656244266b87ac49d06d
FreddyJimenez9000 <33761463+FreddyJimenez9000@users.noreply.github.com>
2018-01-24 09:45:09 -0800

    grtting started

--- Freddy Jimenez Hangman.py
+++ Freddy Jimenez Hangman.py
@@ -1,13 +1,28 @@
-"""
-this is a guide of how to make hangman
+import random
+import string
+# this is a guide of how to make hangman
+#
+# 1. make a word bank - 10 items
+# 2. Select a random guess
+# 3. take in a letter and add it to a list of letters_guess
+# 4. reveal letters based on input
+# 5. create win/lose condition
 
-1. make a word bank - 10 items
-2. Select a random guess
-3. hide the word (use *)
-4. reveal letters based on input
-5. create win/lose condition
-"""
-word = "death", "math", "homework", "money", "games", "stuff", "Family Guy", "school is fun", "Edison Tigers", "Gaston"
-print(len(word))
+word = ['death', 'math', 'homework', 'money', 'games', 'stuff', 'Family Guy', 'school is fun', 'Edison Tigers', 'Gaston']
+choice = random.choice(word)
+
+guess = 10
+strOne = choice
+listOne = list(strOne)
+if guess != 0 :
+    print("let's play a game of hangman")
+    print("there are going to be 10 word that you're going to guess. You will only have 10 tries to guess the word.")
+    print(listOne * 0)
+    stuff = string.ascii_letters
+    stuff = input(int())
+
+
+
+
 
 
