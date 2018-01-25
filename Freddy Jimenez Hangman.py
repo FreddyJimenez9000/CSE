1f44aded7c8fef041ad1f1345f92d5d459f56bc4
FreddyJimenez9000 <33761463+FreddyJimenez9000@users.noreply.github.com>
2018-01-25 09:44:43 -0800

    still thinking

--- Freddy Jimenez Hangman.py
+++ Freddy Jimenez Hangman.py
@@ -9,17 +9,19 @@ import string
 # 5. create win/lose condition
 
 word = ['death', 'math', 'homework', 'money', 'games', 'stuff', 'Family Guy', 'school is fun', 'Edison Tigers', 'Gaston']
-choice = random.choice(word)
 
+choice = random.choice(word)
 guess = 10
 strOne = choice
 listOne = list(strOne)
-if guess != 0 :
+if guess != 0:
     print("let's play a game of hangman")
     print("there are going to be 10 word that you're going to guess. You will only have 10 tries to guess the word.")
-    print(listOne * 0)
+    print(listOne)
+    print(choice)
     stuff = string.ascii_letters
     stuff = input(int())
+
 
 
 
