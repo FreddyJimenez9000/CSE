caaf8ac092d1377b4593f8f7ae9878d858ce74ba
FreddyJimenez9000 <33761463+FreddyJimenez9000@users.noreply.github.com>
2018-01-31 09:44:24 -0800

    fix

--- Freddy Jimenez Hangman.py
+++ Freddy Jimenez Hangman.py
@@ -1,7 +1,6 @@
 import random
 import string
 # this is a guide of how to make hangman
-#
 # 1. make a word bank - 10 items
 # 2. Select a random guess
 # 3. take in a letter and add it to a list of letters_guess
@@ -9,19 +8,35 @@ import string
 # 5. create win/lose condition
 
 word = ['death', 'math', 'homework', 'money', 'games', 'stuff', 'Family Guy', 'school is fun', 'Edison Tigers', 'Gaston']
-choice = random.choice(word)
 guess = 10
-strOne = choice
+strOne = random.choice(word)
+strOne = strOne.lower()
 listOne = list(strOne)
+listTwo = []
+for letter in strOne:
+    if letter != ' ':
+        listTwo.append("*")
+    else:
+        listTwo.append(" ")
+
+print(strOne)
+print(listTwo)
+print("let's play a game of hangman")
+print("you're going to have 10 guess to guess the word.")
 while guess != 0:
-    print("let's play a game of hangman")
-    print("you're are going to have 10 guess to guess the word.")
     print(listOne)
-    break
-print(string.ascii_letters)
-print(string.ascii_lowercase)
-print(listOne)
-l1 = ['d', 'e', 'a', 't', 'h']
-print("". join(l1))
+    print(listTwo)
+    stuff = input("letter >")
+    if listTwo in guess:
+        
 
 
+
+
+
+
+# print(string.ascii_letters)
+# print(string.ascii_lowercase)
+# print(listOne)
+# l1 = ['d', 'e', 'a', 't', 'h']
+# print("". join(l1))
