7775ceb7d7582a6ffd6b78b9d39a6cfa8182911b
FreddyJimenez9000 <33761463+FreddyJimenez9000@users.noreply.github.com>
2018-03-20 09:47:05 -0700

    finish up and hurry

--- Freddy Jimenez - Character.py
+++ Freddy Jimenez - Character.py
@@ -24,33 +24,33 @@ person1 = Character('Henry',
                     "life."
                     "you found this job on ebay? what, What wrong with you ",
                     'Flashlight',
-                    ' H = 100, S = 100',
+                    ' H = 100',
                     None, None, None)
 Bonnie = Character('Bonnie',
                    "Bonnie is a fun bunny which bring joy around the pizzeria. But then again it smell like a dead bod-"
                    "something",
-                   None,', H = 100 s = 0', 'guitar',
+                   None,', H = 100', 'guitar',
                    'play the guitar', "you become blind and paralyzed")
 Freddy = Character('Freddy Fazbear',
                    "Freddy is the most lovable bear in the whole pizzeria. He liked the star of the band. But every"
                    "time he sing it sound like a dead man crying, trying to get out. but who knows.",
-                   None,'H = 100, s = 0','Mic', 'Sing', "you become blind and paralyzed")
+                   None,'H = 100','Mic', 'Sing', "you become blind and paralyzed")
 Chica = Character('Chica', "Chica is a duck like animatronic who soul purpose is to give out cupcake. but we don't "
                   "know what inside of those cupcake maybe bloo-. its made organic.;)",
-                  None, 'H = 100, S = 0', 'Cupcake', "give out cupcakes", "you become blind and paralyzed.")
+                  None, 'H = 100', 'Cupcake', "give out cupcakes", "you become blind and paralyzed.")
 Foxy = Character('Foxy', "Foxy is in the back stage most of the time because he's shy. His fur is always red but it"
                          " used to be white. We think that an employee colored it red. But it started to smell.",
-                 None, 'H = 100, S = 0',"a Hook", None,"you become blind and paralyzed.")
+                 None, 'H = 100',"a Hook", None,"you become blind and paralyzed.")
 Ballora = Character('Ballora', "She a fun charter, like to sing, dance, and to put on a show",
-                    None, "H = 100 S = 0", None, "dance", "you become blind and paralyzed." )
+                    None, "H = 100", None, "dance", "you become blind and paralyzed." )
 Fun_time_Freddy = Character("Fun time Freddy","He's a lovable character which was used in the last pizzeria but run out"
                             "business because of a weird smell and a disappearance of children.",
-                            None, "H = 100 S = 0", None, "play with children", "you become blind and paralyzed.")
+                            None, "H = 100", None, "play with children", "you become blind and paralyzed.")
 Fun_time_Foxy = Character("Fun Time Foxy", "She's the sister of Foxy but have a few improvement over Foxy. But we "
                           "saw something very strange in the old security camera of a child disappearing and Fun time "
-                          "Foxy came out.", None, "H = 100 S = 0", None, "Dance", "you become blind and paralyzed.")
+                          "Foxy came out.", None, "H = 100", None, "Dance", "you become blind and paralyzed.")
 Baby = Character("Baby", "Baby was made to take care and make kids happy. But they started to disappear when they go "
-                         "and play with Baby.", None, "H = 100 S = 0", None,
+                         "and play with Baby.", None, "H = 100", None,
                  "dance and sing", "you become blind and paralyzed." )
 
 print(Freddy.description)
--- Freddy Jimenez - Items.py
+++ Freddy Jimenez - Items.py
@@ -1,16 +1,47 @@
+
 inventory = []
 print("there a mic on the ground")
 command = input(":").lower()
+# head = False
+# chest = False
+# leg = False
+# feet = False
+
 
 class Item(object):
-    def __init__(self, name,desciption, pick_up, room):
+    def __init__(self, name, description, pick_up, room):
         self.name = name
-        self.desciption = desciption
+        self.description = description
         self.Pickup = pick_up
-        self.fromroom = room
+        self.from_room = room
 
 
-    
+class Weapon(Item):
+    def __init__(self, name, description, damage_ratio, room):
+        super(Weapon, self).__init__(name, description, damage_ratio, room)
+        self.damage = damage_ratio
+
+
+class Consumable(Item):
+    def __init__(self, name, description, pickup ,room):
+        super(Consumable, self). __init__(name, description, pickup, room)
+        self.eaten = False
+
+
+class Wearable(Item):
+    def __init__(self, name, description, pickup, room):
+        super(Wearable, self). __init__(name, description, pickup, room)
+
+    def wear(self):
+        if command == "put on %s" % self.name:
+            print("You put on the %s" % self.name)
+
+
+
+
+
+
+
 
     # def Pick_up(self):
     #     if command == "pickup %s" % self.name:
@@ -20,3 +51,9 @@ class Item(object):
     # if command == "inventory":
     #    print(inventory)
 #
+
+
+class Range(Weapon):
+    def __init__(self, name, description, affect, room, damage_ratio):
+        super(Range, self). __init__(name, description, damage_ratio, room )
+        self.affect = affect
